22358fee6d1118176be02564d4692abfe4aa90f4
lx852357 <lx852357@outlook.com>
2019-08-15 16:48:17 +0800

    add AI

--- demo/source/AI.h
+++ demo/source/AI.h
@@ -1,19 +1,38 @@
+#pragma once
 #ifndef _AI_H_
 #define _AI_H_
 
-
-/**
-AI类，负责管理Role类
+#include "message.h"
+/*
+* 1、处理的信息，然后做出决策
+* 2、AI负责管理四个player的行动
 */
+
 class AI
 {
 public:
 	AI();
 	~AI();
+
+	// 输入相邻坐标，返回方向，如果坐标不相邻返回 -1
+	int moveTo(Point target);
+	int moveTo(DIRECT direct);
+
+	void update(); // 更新所有的消息
+	void Vision();
+	void updateState(); // 更新状态机
+	void run();
+
+public:
+	Point mPoint; // 自己的坐标
+	int id;       // 自己的id 和player相同
+
+	enum STATE {
+		SEARCH_MAP, //搜寻地图
+		RUN_AWAY,   // 逃跑
+		ROUND_UP,   //围捕
+		SELF_KILL,  //自杀
+	};
 };
 
-class AI
-{
-};
-
-#endif
+#endif// _AI_H_
