4aac9610b23c6433f8050e05137a9e04300ea9b2
lx <565267033@qq.com>
2019-08-24 20:46:08 +0800

    add state machine in AI

--- client/ballclient/service/AI.py
+++ client/ballclient/service/AI.py
@@ -6,11 +6,34 @@ Created on Wed Aug 21 20:00:57 2019
 """
 from ballclient.service.astar import A_Star
 from ballclient.service.GameMap import gameMap as gameMap
+from ballclient.service.StateMachine import StateMachine
 import math
 class point:
     def __init__(self,x,y):
         self.x=x
         self.y=y
+    
+    def distance(self,p):
+        if math.fabs(self.x-p.x)>math.fabs(self.y-p.y):
+            return math.fabs(self.x-p.x)
+        else:
+            return math.fabs(self.y-p.y)
+        
+class Transition:
+    def __init__(self):
+        self.N = 0
+        self.P = 1
+        self.SEE = 2
+        self.BIGFISH = 16
+        
+class State:
+    def __init__(self):
+        self.START = "START_STATE"
+        self.RUNAWAY = "RUNAWAY_STATE"
+        self.SEARCH = "SEARCH_STATE"
+        self.CATCH = "CATCH_STATE"
+        self.ERROR = "ERROR_STATE"
+
 class AI:
     def __init__(self, id,vision,name):
         self.isvalid=True
@@ -23,12 +46,155 @@ class AI:
         self.target=point(0,0)
         self.name=name
         
+        #保存看到过但还没吃的分值 
+        self.seePowers = []
+        
+        #状态转移条件
+
+        self.nowTState = 0 # P|SEE 
+        #初始化状态机
+        self.stateMachine = StateMachine()
+        #转移条件 
+        self.t = Transition()
+        #状态转移加入到状态机中
+        self.s = State()
+        self.stateMachine.add_state(self.s.START,self.origin_transition)
+        self.stateMachine.add_state(self.s.SEARCH,self.search_transition)
+        self.stateMachine.add_state(self.s.RUNAWAY,self.runaway_transition)
+        self.stateMachine.add_state(self.s.CATCH,self.catch_transition)
+        self.stateMachine.add_state(self.s.ERROR, None, end_state=1)#终止状态
+        self.stateMachine.set_start(self.s.START)#设置初始状态
+        
+        #状态动作函数，在某个状态执行某个函数
+        self.stateAction = {}
+        self.stateAction[self.s.START] = self.start
+        self.stateAction[self.s.SEARCH] = self.search
+        self.stateAction[self.s.RUNAWAY] = self.runaway
+        self.stateAction[self.s.CATCH] = self.catch
+        
+        self.test = 0#测试用的
+        
+
+        
     def update(self):
         self.isvalid=False
         for pinfo in gameMap.ourCurrentPlayer:
             if pinfo[0]==self.id:
                 self.isvalid=True
                 self.mpoint=point(pinfo[1],pinfo[2])
+                
+        #todo 我想把power all放在这个里面来，但是放进来后机器人就不动了，暂时没找到bug在哪
+        '''power all'''
+        '''
+        self.powers.clear()
+        
+        for cur_power in gameMap.curpowers:
+            if(self.mpoint.distance(point(cur_power[1],cur_power[2])) <= self.vision):
+                if cur_power not in self.seePowers:
+                    self.seePowers.append(cur_power)
+                self.powers.append(point(cur_power[1],cur_power[2]))
+                gameMap.curpowers.remove(cur_power) #从中取走这个点，避免和其他机器人冲突
+                print ("new ",self.name,": ",cur_power)
+                print("gCurpowers:",gameMap.curpowers)
+                break
+        #吃分检测
+        for power in self.seePowers:
+            if self.mpoint.distance(point(power[1],power[2])) <= self.vision:
+                if power not in gameMap.curpowers: #吃到分了
+                    self.seePowers.remove(power)
+                else:
+                    #no nothing
+        '''
+        
+        
+        #todo 这个更新状态还没做，参考run下面的代码做状态更新
+        '''更新状态'''
+        
+        
+        
+        
+    #状态转移
+    def origin_transition(self,TState):
+    
+        t = TState & 0x0f #取低四位
+        if(t == self.t.P):
+            newState = self.s.SEARCH
+        elif(t == self.t.N):
+            newState = self.s.SEARCH
+        elif(t == self.t.SEE):
+             newState = self.s.RUNAWAY
+        elif(t == self.t.P | self.t.SEE):
+            newState = self.s.SEARCH
+        else:
+            newState = "error_state"
+        print("origin state->",newState)
+        return newState
+
+    #search score state
+    def search_transition(self,TState):
+        
+        t = TState & 0x0f #取低四位
+        #这里要注意一下，没用低四位，而是加了可选择位，其他的elif用的低四位
+        if(TState == self.t.P | self.t.BIGFISH | self.t.SEE):
+            newState = self.s.CATCH
+        elif(t == self.t.N):
+            newState = self.s.SEARCH
+        elif(t == self.t.SEE):
+             newState = self.s.RUNAWAY
+        elif(t == self.t.P | self.t.SEE):
+            newState = self.s.SEARCH
+        else:
+            newState = "error_state"
+        print("search_score_state->",newState)
+        return newState
+    
+    def runaway_transition(self,TState):
+        t = TState & 0x0f #取低四位
+        if(TState == self.t.P | self.t.BIGFISH | self.t.SEE):
+            newState = self.s.CATCH
+        elif(t == self.t.N):
+            newState = self.s.SEARCH
+        elif(t == self.t.SEE):
+             newState = self.s.RUNAWAY
+        elif(t == self.t.P | self.t.SEE):
+            newState = self.s.SEARCH
+        else:
+            newState = "error_state"
+        print("run_away_state->",newState)
+    
+        return newState
+    
+    def catch_transition(self,TState):
+        t = TState & 0x0f #取低四位
+        if(TState == self.t.P | self.t.BIGFISH | self.t.SEE):
+            newState = self.s.CATCH
+        elif(t == self.t.N):
+            newState = self.s.SEARCH
+        elif(t == self.t.SEE):
+             newState = self.s.RUNAWAY
+        elif(t == self.t.P | self.t.SEE):
+            newState = self.s.SEARCH
+        else:
+            newState = "error_state"
+        print("catch_state->",newState)
+    
+        return newState
+    
+    #state action 在某个状态下做什么事，就在这里写了
+    def search(self):
+        print("Do search")
+        
+    def catch(self):
+        print("Do catch")
+        
+    def runaway(self):
+        print("Do runaway")
+        
+    def start(self):
+        print("Do start")
+        
+            
+        
         
     def run(self):
         if len(self.powers):
@@ -44,7 +210,20 @@ class AI:
             self.vis_num=self.vis_num+1
             if self.vis_num>=len(self.waypoint):
                 self.vis_num=0
-
+        
+        
+        #这段代码是测试代码
+        #测试序列 理论上转移 origin state-> SEARCH_STATE -> CATCH -> RUNAWAY_STATE
+        test_queue = [(self.t.P | self.t.SEE),(self.t.P | self.t.SEE | self.t.BIGFISH),self.t.SEE]
+        
+        self.test = self.test + 1
+        self.test = self.test%3
+        print("test: ",self.test)
+        self.stateMachine.run(test_queue[self.test])
+        Do = self.stateAction[self.stateMachine.nowState]
+        Do()
+        #这段代码是测试代码
+        
       
 class Team:
     def __init__(self):
@@ -52,24 +231,24 @@ class Team:
         self.idiot=AI(gameMap.ourPlayer[1],gameMap.vision,'idiot')
         self.fool=AI(gameMap.ourPlayer[2],gameMap.vision,'fool')
         self.git=AI(gameMap.ourPlayer[3],gameMap.vision,'git')
-        self.stupid.waypoint.append(point(3,13))
-        self.stupid.waypoint.append(point(3,8))
-        self.stupid.waypoint.append(point(3,3))
-        self.stupid.waypoint.append(point(8,3))
-        self.stupid.waypoint.append(point(13,3))
+        self.stupid.waypoint.append(point(4,18))
+        #self.stupid.waypoint.append(point(3,8))
+        #self.stupid.waypoint.append(point(3,3))
+        #self.stupid.waypoint.append(point(8,3))
+        self.stupid.waypoint.append(point(4,4))
         
-        self.idiot.waypoint.append(point(16,3))
-        self.idiot.waypoint.append(point(16,8))
-        self.idiot.waypoint.append(point(16,13))
-        self.idiot.waypoint.append(point(16,16))
+        self.idiot.waypoint.append(point(3,6))
+        self.idiot.waypoint.append(point(11,6))
+        self.idiot.waypoint.append(point(3,16))
+       # self.idiot.waypoint.append(point(16,16))
         
-        self.fool.waypoint.append(point(11,8))
-        self.fool.waypoint.append(point(10,10))
-        self.fool.waypoint.append(point(8,11))
+        self.fool.waypoint.append(point(5,16))
+        self.fool.waypoint.append(point(11,16))
+        self.fool.waypoint.append(point(16,16))
         
-        self.git.waypoint.append(point(13,16))
-        self.git.waypoint.append(point(8,16))
-        self.git.waypoint.append(point(3,16))
+        self.git.waypoint.append(point(16,18))
+        self.git.waypoint.append(point(16,4))
+        #self.git.waypoint.append(point(3,16))
         self.a_star = A_Star(0, 0, 0, 0,gameMap.height,gameMap.width)
     
     def update(self):
@@ -80,7 +259,7 @@ class Team:
         
     def process(self):
         self.update()
-        self.power_all()
+        #self.power_all()
         self.stupid.run()
         self.idiot.run()
         self.fool.run()
@@ -115,16 +294,21 @@ class Team:
         for cur_power in gameMap.curpowers:
             if getdis(self.stupid.mpoint,point(cur_power[1],cur_power[2]))<=self.stupid.vision:
                 self.stupid.powers.append(point(cur_power[1],cur_power[2]))
+                #print ("stupid power: ",cur_power)
                 continue
             if getdis(self.idiot.mpoint,point(cur_power[1],cur_power[2]))<=self.idiot.vision:
                 self.idiot.powers.append(point(cur_power[1],cur_power[2]))
+                #print ("idiot power: ",cur_power)
                 continue
             if getdis(self.fool.mpoint,point(cur_power[1],cur_power[2]))<=self.fool.vision:
                 self.fool.powers.append(point(cur_power[1],cur_power[2]))
+                #print ("fool power: ",cur_power)
                 continue
             if getdis(self.git.mpoint,point(cur_power[1],cur_power[2]))<=self.git.vision:
                 self.git.powers.append(point(cur_power[1],cur_power[2]))
+                #print ("git power: ",cur_power)
                 continue
+            
         
 def getdis(p1,p2):
     if math.fabs(p1.x-p2.x)>math.fabs(p1.y-p2.y):
--- client/ballclient/service/GameMap.py
+++ client/ballclient/service/GameMap.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # encoding:utf8
 #import numpy as np
 import copy
@@ -31,6 +30,7 @@ class GameMap(object):
         
         #[score1,x1,y1],[score2,x2,y2]
         self.powers  = []
+        self.curpowers = []
         
         #{name:[[x1,y1],[x2,y2]]}
         #{97: [[6, 13], [19, 0]], 98: [[0, 19], [13, 6]]}
@@ -148,205 +148,7 @@ class GameMap(object):
         self.map2 = copy.deepcopy(self.map)
         self.oppPlayer.clear()
         self.ourCurrentPlayer.clear()
-        
-        '''round 必有的信息'''
-        self.currentMode = msg['msg_data']['mode']
-        self.roundID     = msg['msg_data']['round_id']
-        
-        try:
-            '''power坐标'''
-            powers = msg['msg_data']['power']
-            for power in powers:
-                l = [power['point'],power['x'],power['y']]
-                self.map2[power['y']][power['x']] = power['point']
-                if l not in self.powers:
-                    self.powers.append(l)
-           # print(self.powers)
-        except:
-            pass
-        
-        try:
-            '''player坐标'''
-            players = msg['msg_data']['players']
-            for player in players:
-                self.map2[player['y']][player['x']] = player['id']+10
-                if player['team'] != constants.team_id: 
-                    opp = [player['id'],player['x'],player['y']]
-                    self.oppPlayer.append(opp)
-                else:
-                    our = [player['id'],player['x'],player['y']]
-                    self.ourCurrentPlayer.append(our)
-            #print (self.oppPlayer)
-        except:
-            pass     
-    
-    def isOurPower(self):
-        return self.currentMode == self.ourMode
-gameMap = GameMap()
-'''
-if __name__ == '__main__':
-    round_msg = {"msg_data":{"mode":"think","players":[{"id":4,"score":2,"sleep":0,"team":1112,"x":13,"y":7},{"id":5,"score":6,"sleep":0,"team":1112,"x":13,"y":2},{"id":6,"score":4,"sleep":0,"team":1112,"x":1,"y":13},{"id":7,"score":4,"sleep":0,"team":1112,"x":17,"y":4}],"power":[{"point":1,"x":12,"y":2},{"point":1,"x":16,"y":2},{"point":1,"x":17,"y":3},{"point":1,"x":10,"y":8},{"point":2,"x":11,"y":8},{"point":5,"x":10,"y":9},{"point":1,"x":11,"y":9},{"point":1,"x":2,"y":12},{"point":1,"x":2,"y":16}],"round_id":146,"teams":[{"id":1111,"point":24,"remain_life":4},{"id":1112,"point":16,"remain_life":4}]},"msg_name":"round"}
-    start_msg  = {"msg_data":{"map":{"height":20,"meteor":[{"x":18,"y":1},{"x":19,"y":1},{"x":7,"y":4},{"x":8,"y":4},{"x":11,"y":4},{"x":12,"y":4},{"x":4,"y":7},{"x":15,"y":7},{"x":4,"y":8},{"x":15,"y":8},{"x":4,"y":11},{"x":15,"y":11},{"x":4,"y":12},{"x":15,"y":12},{"x":7,"y":15},{"x":8,"y":15},{"x":11,"y":15},{"x":12,"y":15},{"x":0,"y":18},{"x":1,"y":18}],"tunnel":[{"direction":"up","x":5,"y":5},{"direction":"right","x":6,"y":5},{"direction":"right","x":7,"y":5},{"direction":"right","x":8,"y":5},{"direction":"right","x":9,"y":5},{"direction":"right","x":10,"y":5},{"direction":"right","x":11,"y":5},{"direction":"right","x":12,"y":5},{"direction":"right","x":13,"y":5},{"direction":"right","x":14,"y":5},{"direction":"up","x":5,"y":6},{"direction":"down","x":14,"y":6},{"direction":"up","x":5,"y":7},{"direction":"down","x":14,"y":7},{"direction":"up","x":5,"y":8},{"direction":"down","x":14,"y":8},{"direction":"up","x":5,"y":9},{"direction":"down","x":14,"y":9},{"direction":"up","x":5,"y":10},{"direction":"down","x":14,"y":10},{"direction":"up","x":5,"y":11},{"direction":"down","x":14,"y":11},{"direction":"up","x":5,"y":12},{"direction":"down","x":14,"y":12},{"direction":"up","x":5,"y":13},{"direction":"down","x":14,"y":13},{"direction":"left","x":5,"y":14},{"direction":"left","x":6,"y":14},{"direction":"left","x":7,"y":14},{"direction":"left","x":8,"y":14},{"direction":"left","x":9,"y":14},{"direction":"left","x":10,"y":14},{"direction":"left","x":11,"y":14},{"direction":"left","x":12,"y":14},{"direction":"left","x":13,"y":14},{"direction":"down","x":14,"y":14},{"direction":"left","x":8,"y":16},{"direction":"left","x":9,"y":16},{"direction":"left","x":10,"y":16}],"vision":3,"width":20,"wormhole":[{"name":"A","x":19,"y":0},{"name":"b","x":13,"y":6},{"name":"a","x":6,"y":13},{"name":"B","x":0,"y":19}]},"teams":[{"force":"beat","id":1111,"players":[0,1,2,3]},{"force":"think","id":1112,"players":[4,5,6,7]}]},"msg_name":"leg_start"}
-
-    m = GameMap()
-    m.handleMsg(start_msg)
-    m.updateMsg(round_msg)
-    m.printAll()
-    plt.figure('map2')
-    plt.imshow(m.map2)
-    plt.figure('map')
-    plt.imshow(m.map)
-=======
-# encoding:utf8
-#import numpy as np
-import copy
-#import matplotlib.pyplot as plt
-import ballclient.service.constants as constants
-#import constants as constants
-'''
-* 全局地图
-* 1、因为涉及到类之间的交互，
-* 2、当搜集到得分点后，需要管理得分点的坐标，全局地图只展示地图信息
-* 3、全局地图中维护一个动态地图，作为场地当前信息
-* 4、全局地图只有一个，因此使用单例
-
-
-0：地图可行区域
-1 - 5：分数奖励
-10 - 17：player信息
-8：障碍物 
-虫洞id的ascii码：表示虫洞对
-20 - 23表示滑梯的上下左右
-'''
-class GameMap(object):
-
-    def __init__(self):
-
-        self.height  = 0
-        self.width   = 0
-        self.map     = 0
-        self.map2    = 0
-        
-        #[score1,x1,y1],[score2,x2,y2]
-        self.powers  = []
-        self.curpowers=[]        
-        #{name:[[x1,y1],[x2,y2]]}
-        #{97: [[6, 13], [19, 0]], 98: [[0, 19], [13, 6]]}
-        self.wormholePair = {}
-        
-        #[[direct, x, y], [direct, x, y]]
-        #[[20, 5, 5], [23, 6, 5]
-        self.tunnels = []
-        
-        self.ourCurrentPlayer = []
-        self.ourPlayer    = []#[0,1,2,3] or [4,5,6,7]
-        self.oppPlayer    = []#[0,1,2,3] or [4,5,6,7]
-        
-        self.vision       = 0
-        self.roundID      = 0
-        self.ourMode      = 0   #表示我方能力 think，beat
-        self.currentMode  = 0   #表示当前优势能力 think，beat
-        
-    def printAll(self):
-        print ('h:',self.height,'w:',self.width)
-        print ('powers: ',self.powers)
-        print ('wormholePair: ',self.wormholePair)
-        print ('tunnels: ', self.tunnels)
-        print ('ourPlayer: ', self.ourPlayer)
-        print ('vision: ', self.vision)
-        print ('roundID: ', self.roundID)
-        print ('isourPower? ', self.isOurPower())
-        print ('oppPlayer: ', self.oppPlayer)
-        print ('ourCurrentPlayer: ', self.ourCurrentPlayer)
- 
-    '''处理leg_start消息'''
-    def handleMsg(self, msg):
-
-        '''初始化地图'''
-        self.width  = msg['msg_data']['map']['width']
-        self.height = msg['msg_data']['map']['height']
-        self.map    = [[0 for col in range(self.width)] for row in range(self.height)]
-        self.map2   = [[0 for col in range(self.width)] for row in range(self.height)]
-
-        self.vision = msg['msg_data']['map']['vision']
-        
-        
-        '''生成陨石坐标'''
-        meteor = msg['msg_data']['map']['meteor']
-        for dic in meteor:
-            y = 0
-            x = 0
-            for k,v in dic.items():
-                if(k=='y'):
-                    y = v
-                if(k=='x'):
-                    x = v
-            self.map[y][x] = 8 
-        
-        '''tunnel坐标'''
-        tunnels = msg['msg_data']['map']['tunnel']
-        for tunnel in tunnels:
-            if tunnel['direction'] == 'up':
-                d = 20
-            if tunnel['direction'] == 'down':
-                d = 21          
-            if tunnel['direction'] == 'left':
-                d = 22            
-            if tunnel['direction'] == 'right':
-                d = 23
-            
-            self.map[tunnel['y']][tunnel['x']] = d
-            self.tunnels.append([d,tunnel['x'],tunnel['y']])
-
-        
-                    
-        '''wormhole坐标'''
-        wormhole = msg['msg_data']['map']['wormhole']
-        wh = []
-        for dic in wormhole:
-            temp = []
-            x = 0
-            y = 0
-            name = 0
-            for k,v in dic.items():
-                if(k=='y'):
-                    y = v
-                elif(k=='x'):
-                    x = v
-                else:
-                    name = ord(v.lower())
-            
-            if len(wh):
-                for l in wh:
-                    if(l[0] == name):
-                        self.map[y][x] = l[0]
-                        self.map[l[1]][l[2]] = l[0]
-                        self.wormholePair[l[0]] = [[x,y],[l[2],l[1]]]
-                        wh.remove(l)
-                    else:
-                        temp = [name,y,x]
-            else:
-                wh.append([name,y,x])
-            if len(temp):
-                wh.append(temp)
-
-        
-        #ourplayer = []
-        '''ourplayer info'''
-        teams = msg['msg_data']['teams']
-        for team in teams:
-            if team['id'] == constants.team_id:
-                self.ourPlayer = team['players']
-                self.ourMode   = team['force']
-
-                
-                    
-    '''更新地图消息'''
-    def updateMsg(self, msg):
-        self.map2 = copy.deepcopy(self.map)
-        self.oppPlayer.clear()
         self.curpowers.clear()
-        self.ourCurrentPlayer.clear()
         
         '''round 必有的信息'''
         self.currentMode = msg['msg_data']['mode']
@@ -357,8 +159,9 @@ class GameMap(object):
             powers = msg['msg_data']['power']
             for power in powers:
                 l = [power['point'],power['x'],power['y']]
-                self.curpowers.append(l)
+                cl= [power['point'],power['x'],power['y']]
                 self.map2[power['y']][power['x']] = power['point']
+                self.curpowers.append(cl)
                 if l not in self.powers:
                     self.powers.append(l)
            # print(self.powers)
@@ -396,5 +199,4 @@ if __name__ == '__main__':
     plt.imshow(m.map2)
     plt.figure('map')
     plt.imshow(m.map)
->>>>>>> 9065546fe515532ccd791d205345335bd2876f06
 '''
